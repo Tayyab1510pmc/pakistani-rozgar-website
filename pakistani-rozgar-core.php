9f238a33ad54cc046800265156fab470b9bfde0d
Muhammad Tayyab Javed <tayyab.javed.b28@pmc.edu.pk>
2026-04-15 20:00:29 +0500

    Update pakistani-rozgar-core.php to version 6.0

--- pakistani-rozgar-core.php
+++ pakistani-rozgar-core.php
@@ -1,1 +1,10 @@
-\n// Updated at 2026-04-15 14:26:13 UTC for testing WP Pusher auto-deployment connection\n
+<?php
+/**
+ * Plugin Name: Pakistani Rozgar Core
+ * Description: A core plugin for the Pakistani Rozgar website.
+ * Version: 6.0
+ * Author: Tayyab1510pmc
+ */
+
+ // Your plugin code goes here
+
